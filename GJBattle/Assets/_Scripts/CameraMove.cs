958532be0cf2eae608f78ce8304269158329aa69
RENCUREL <pierre.rencurel@orange.fr>
2019-05-25 10:04:34 -0400

    Zoom Exploratory
    ADDED : First iteration to zoom (non functional)
    TODO : Finalize zoom

--- GJBattle/Assets/_Scripts/CameraMove.cs
+++ GJBattle/Assets/_Scripts/CameraMove.cs
@@ -32,7 +32,7 @@ public class CameraMove : MonoBehaviour
     void LateUpdate()
     {
         MouseMove();
-        
+        CameraZoom();
     }
 
     /// <summary>
@@ -100,6 +100,29 @@ public class CameraMove : MonoBehaviour
                     transform.Translate(move, Space.World);
                 }
             }
+        }
+    }
+
+    void CameraZoom()
+    {
+        ///<summary>
+        ///ScrollWheel change input
+        ///</summary>
+        float ScrollWheelChange = Input.GetAxis("Mouse ScrollWheel");
+        if (ScrollWheelChange != 0)
+        {                                            //If the scrollwheel has changed
+            float R = ScrollWheelChange * 15;                                   //The radius from current camera
+            float PosX = Camera.main.transform.eulerAngles.x + 90;              //Get up and down
+            float PosY = -1 * (Camera.main.transform.eulerAngles.y - 90);       //Get left to right
+            PosX = PosX / 180 * Mathf.PI;                                       //Convert from degrees to radians
+            PosY = PosY / 180 * Mathf.PI;                                       //^
+            float X = R * Mathf.Sin(PosX) * Mathf.Cos(PosY);                    //Calculate new coords
+            float Z = R * Mathf.Sin(PosX) * Mathf.Sin(PosY);                    //^
+            float Y = R * Mathf.Cos(PosX);                                      //^
+            float CamX = Camera.main.transform.position.x;                      //Get current camera postition for the offset
+            float CamY = Camera.main.transform.position.y;                      //^
+            float CamZ = Camera.main.transform.position.z;                      //^
+            Camera.main.transform.position = new Vector3(CamX + X, CamY + Y, CamZ + Z);//Move the main camera
         }
     }
 }
