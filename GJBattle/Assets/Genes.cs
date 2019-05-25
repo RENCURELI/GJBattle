6162b9a593d1155ac677108fff25ddce20b2cfd5
MarcOwl <mviboux@hotmail.com>
2019-05-25 15:22:23 -0400

    J'ai commencer a travailler sur les Billys

---
+++ GJBattle/Assets/Genes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Genes : MonoBehaviour
+{
+	//	l'array qui traque les traits des billy
+	public int[] traits;
+
+    //	Je vais donner des traits arbitraire pour les tests
+    void Start()
+    {
+		traits = new int[3];
+
+        traits[0] = 1;
+		traits[1] = 2;
+		traits[2] = 1;
+
+		Debug.Log(traits.ToString());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
