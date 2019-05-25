bf892b570b1c722044598a766216e32bbc9cb093
MarcOwl <mviboux@hotmail.com>
2019-05-25 18:01:55 -0400

    Je travail sur les traits.

--- GJBattle/Assets/_Scripts/CellsVisual/Genes.cs
+++ GJBattle/Assets/_Scripts/CellsVisual/Genes.cs
@@ -12,17 +12,10 @@ public class Genes : MonoBehaviour
     {
 		traits = new int[3];
 
-        traits[0] = 1;
-		traits[1] = 2;
+        traits[0] = 2;
+		traits[1] = 1;
 		traits[2] = 1;
 
-		Debug.Log(traits.ToString());
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
---
+++ GJBattle/Assets/_Scripts/CellsVisual/OCSSprite.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OCSSprite : MonoBehaviour
+{
+    //	La selection de sprite utilise pour le visuel des Billy
+	public Sprite traitR;
+	public Sprite traitB;
+	public Sprite traitG;
+
+	//	Lequelle des trois traits herite ce genome
+	public int spritePosition;
+
+	void Start()
+    {
+        if (this.gameObject.Tag == "Orbit") 
+		{
+			spritePosition = gameObject.GetComponentInParent(Genes).traits[0];
+
+		}
+		
+		//gameObject.GetComponentInParent();
+
+    }
+
+    
+
+}
