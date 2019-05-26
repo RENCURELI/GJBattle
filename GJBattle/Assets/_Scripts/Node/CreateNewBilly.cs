c2a0509996c9d6b4e9995054f1c59c7dcd7bc970
MarcOwl <mviboux@hotmail.com>
2019-05-26 08:56:24 -0400

    J'ai gosser sur la creation de Billy aleatoire.

--- GJBattle/Assets/_Scripts/Node/CreateNewBilly.cs
+++ GJBattle/Assets/_Scripts/Node/CreateNewBilly.cs
@@ -4,10 +4,45 @@ using UnityEngine;
 
 public class CreateNewBilly : MonoBehaviour
 {
+	
+	private int[] chaos1;
+	private int[] chaos2;
+	private int[] chaos3;
+	private int[] chaos4;
+	private int[] chaos5;
+	private int[] chaos6;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("THE MACHINE");
+
+		chaos1 = new int[3];
+
+		chaos1[3] = Random.Range (0,1);
+
+		chaos2 = new int[3];
+
+		chaos2[3] = Random.Range (1,2);
+
+		chaos3 = new int[3];
+
+		chaos3[3] = Random.Range (1,2);
+
+		chaos4 = new int[3];
+
+		chaos4[3] = Random.Range (0,2);
+
+		chaos5 = new int[3];
+
+		chaos5[3] = Random.Range (0,1);
+
+		chaos6 = new int[3];
+
+		chaos6[3] = Random.Range (0,1);
+
+		
+
     }
 
     // Update is called once per frame
@@ -15,7 +50,9 @@ public class CreateNewBilly : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("Hello: ");
+            Debug.Log("5 Billy");
+
+
         }
     }
 }
